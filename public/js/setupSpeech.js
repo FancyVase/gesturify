e3427523ae7aee6a0e641c3d6d9b2fcedbfe5aa6
Maryam Archie <marchie@mit.edu>
2018-05-14 01:34:05 -0400

    Use spaces instead of tabs

--- public/js/setupSpeech.js
+++ public/js/setupSpeech.js
@@ -7,28 +7,28 @@ var recognition = new webkitSpeechRecognition();
 recognition.continuous = true;
 recognition.interimResults = true;
 recognition.onresult = function(event) {
-	// Build the interim transcript, so we can process speech faster
-	var transcript = '';
-	var hasFinal = false;
-	for (var i = event.resultIndex; i < event.results.length; ++i) {
-		if (event.results[i].isFinal)
-			hasFinal = true;
-		else
-			transcript += event.results[i][0].transcript;
-	}
+  // Build the interim transcript, so we can process speech faster
+  var transcript = '';
+  var hasFinal = false;
+  for (var i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal)
+      hasFinal = true;
+    else
+      transcript += event.results[i][0].transcript;
+  }
 
-	var processed = debouncedProcessSpeech(transcript);
+  var processed = debouncedProcessSpeech(transcript);
 
   // If we reacted to speech, kill recognition and restart
   if (processed) {
-  	recognition.stop();
+    recognition.stop();
   }
 };
 // Restart recognition if it has stopped
 recognition.onend = function(event) {
-	setTimeout(function() {
-		recognition.start();
-	}, 1000);
+  setTimeout(function() {
+    recognition.start();
+  }, 1000);
 };
 recognition.start();
 /*****************************************************************/
